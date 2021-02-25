53c9ef301d6a61ecc7099add5bbc0dd9a8de75f1
adsonatural <adsonhenriquesilva@gmail.com>
2021-02-25 12:56:58 +0100

    fix

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -6,13 +6,16 @@ export function ExperienceBar() {
     const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
     const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+    console.log(percentToNextLevel);
+    console.log(currentExperience);
+    console.log(experienceToNextLevel);
 
     return (
         <header className={styles.experienceBar}>
             <span>0 XP</span>
             <div>
-                <div style={{ width: `${percentToNextLevel}` }} />
-                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}` }}>
+                <div style={{ width: `${percentToNextLevel}%` }} />
+                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
                     {currentExperience} XP
                 </span>
             </div>
